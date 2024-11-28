f482a2a7282eac15fd102cfe3954638c302b29bc
PotterWalker <139759355+PotterWalker@users.noreply.github.com>
2024-11-28 13:03:26 +0100

    Add files via upload

---
+++ tp.py
@@ -0,0 +1,25 @@
+# Je déclare une variable a, b, c, d, e, f et g
+
+a = 17 + 11
+print(a)
+
+b = 15 - 199
+print(b)
+
+c = 999/9
+print(c)
+
+d = 20%2
+print(d)
+
+
+e = 16//3
+print(e)
+
+f = a + b
+print(f)
+
+g = a + 2
+print(g)
+
+print(g==a)
